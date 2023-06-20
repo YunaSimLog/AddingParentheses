e969b30b76290ba750a1199ef1986a87a9ac6522
yunasim <ynsim@solidit.co.kr>
2023-06-21 00:43:32 +0900

    #✨feature: 초기 소스 업로드

---
+++ AddingParentheses/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingParentheses
+{
+    internal class Program
+    {
+        static int m_nCount = 0;
+        static string m_strEquation = "";
+
+        static void Main(string[] args)
+        {
+            ReadData();
+
+        }
+
+        private static bool ReadData()
+        {
+            try
+            {
+                string strCount = Console.ReadLine();
+                bool bResult = Int32.TryParse(strCount, out m_nCount);
+                if (!bResult)
+                {
+                    Console.WriteLine("올바른 숫자 값을 입력하세요.");
+                    return false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+                Debug.Assert(false);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
